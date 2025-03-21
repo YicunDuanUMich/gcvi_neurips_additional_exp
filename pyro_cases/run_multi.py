dece0ee2d3f86b030a4058499f1d0714c36fb49d
Yicun Duan <pduan@deeplearning-01.stat.lsa.umich.edu>
2025-03-21 16:03:16 -0400

    update great lake config

--- pyro_cases/run_multi.py
+++ pyro_cases/run_multi.py
@@ -4,6 +4,7 @@ import multiprocessing
 import gc
 
 from pathlib import Path
+from termcolor import colored
 
 from pyro_cases.run import train_and_test
 
@@ -17,19 +18,34 @@ if __name__ == "__main__":
     model_num = 100
     task_names = ["gaussian_linear"]
     lr_schedulers = ["plain", 
-                     "custom_decrease", "exponential", "milestones"
+                     "custom_decrease", "exponential", "milestones",
                      "cosine_annealing", "cyclic", "one_cycle", 
                      "cosine_annealing_warm_restart"]
     network_widths = [512, 1024, 2048]
     save_path = Path("/data/scratch/pduan/new_gcvi_output")
-    cuda_devices = [f"cuda:{i}" for i in [1, 2, 3, 4]]
+    cuda_devices = [f"cuda:{i}" for i in [2, 3, 4, 7]]
 
-    task_tags = [(f"t_{tn}_lr_{lr_s}_nw_{network_width}", tn, lr_s, network_width)
+    print_green = lambda x: print(colored(x, "green"))
+
+    print_green("+" * 100)
+    print_green("Config:")
+    print_green(f"\t tasks_names: {task_names}")
+    print_green(f"\t lr_schedulers: {lr_schedulers}")
+    print_green(f"\t network_widths: {network_widths}")
+    print_green(f"\t save_path: {save_path}")
+    print_green(f"\t cuda_devices: {cuda_devices}")
+    print_green(f"\t tags:")
+    task_tags = [(f"t_{tn}_lr_{lr_s}_nw_{network_width}", 
+                  (tn, lr_s, network_width))
                  for tn in task_names
                  for lr_s in lr_schedulers
                  for network_width in network_widths]
-    tasks = {t: [] for t, _, _, _ in task_tags}
-    for t, tn, lr_s, network_width in task_tags:
+    for i, (t, _) in enumerate(task_tags):
+        print_green(f"\t  [{i}]: {t}")
+    print_green("+" * 100)
+
+    tasks = {t: [] for t, _ in task_tags}
+    for t, (tn, lr_s, network_width) in task_tags:
         for model_i in range(model_num):
             tasks[t].append(
                 (train_and_test, 
@@ -47,17 +63,21 @@ if __name__ == "__main__":
                 })
             )
     
+    print_green("create process pools")
     pools = {cuda_d: multiprocessing.Pool(processes=4) for cuda_d in cuda_devices}
-    processes = {t: [] for t, _, _, _ in task_tags}
+    print_green("submit tasks")
+    processes = {t: [] for t, _ in task_tags}
     for tk, tv_list in tasks.items():
         for i, (func, param_dict) in enumerate(tv_list):
             device = cuda_devices[i % len(cuda_devices)]
             pool = pools[device]
             processes[tk].append(pool.apply_async(my_worker, 
                                                   args=(func, param_dict, device)))
+    print_green("have submitted all the tasks")
 
-    for t, _, _, _ in task_tags:
+    for t, _ in task_tags:
         outputs = [p.get() for p in processes[t]]
+        print_green(f"tag {t} completes")
         torch.save(outputs, save_path / f"pyro_{t}_mn_{model_num}.pt")
         del outputs
         gc.collect()
